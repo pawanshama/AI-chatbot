9c8432f91a44074dfb138f5223cc76b169c2a164
pawan sharma <pawansharma02020200@gmail.com>
2025-07-28 13:01:33 +0530

    updated llamaIndex file

--- backend/src/llamaIndex.ts
+++ backend/src/llamaIndex.ts
@@ -2,21 +2,82 @@ import {
   LlamaParseReader,
   // we'll add more here later
 } from "llamaindex";
-import 'dotenv/config'
+// import 'dotenv/config'
 import express from "express";
 const app = express.Router();
+// async function main() {
+//     // save the file linked above as sf_budget.pdf, or change this to match
+//     const path = "./src/cover_letter.docx";
+
+//   // set up the llamaparse reader
+//   const reader = new LlamaParseReader({ resultType: "markdown",parse_mode: "parse_page_with_lvm" });
+
+//   // parse the document
+//   const documents = await reader.loadData(path);
+
+//   // print the parsed document
+//   console.log(documents)
+// }
+
+// main().catch(console.error);
+
+
+import fs from "node:fs/promises";
+
+import { openai, OpenAIEmbedding } from "@llamaindex/openai";
+import {
+  Document,
+  MetadataMode,
+  NodeWithScore,
+  Settings,
+  VectorStoreIndex,
+} from "llamaindex";
+
+Settings.llm = openai({
+  apiKey: process.env.OPENAI_API_KEY,
+  model: "gpt-4o",
+});
+Settings.embedModel = new OpenAIEmbedding();
+
 async function main() {
-    // save the file linked above as sf_budget.pdf, or change this to match
-    const path = "./src/image.png";
+  // Load essay from abramov.txt in Node
+  // const path = "src/cover_letter.docx";
 
-  // set up the llamaparse reader
-  const reader = new LlamaParseReader({ resultType: "markdown" });
+  // const essay = await fs.readFile(path, "utf-8");
 
-  // parse the document
-  const documents = await reader.loadData(path);
+  // Create Document object with essay
+  // const document = new Document({ text: essay, id_: path });
+  // const reader = new LlamaParseReader({ resultType: "markdown",parse_mode: "parse_page_with_lvm" });
+  // const document = await reader.loadData(path);
 
-  // print the parsed document
-  console.log(documents)
+  // Split text and create embeddings. Store them in a VectorStoreIndex
+  // const index = await VectorStoreIndex.fromDocuments(document);
+
+  //This is new code to parse a document
+  const path = "node_modules/llamaindex/examples/abramov.txt";
+  const essay = await fs.readFile(path, "utf-8");
+  // Create Document object with essay
+  const document = new Document({ text: essay, id_: path });
+  // Split text and create embeddings. Store them in a VectorStoreIndex
+  const index = await VectorStoreIndex.fromDocuments([document]);
+
+
+  // Query the index
+  const queryEngine = index.asQueryEngine();
+  const { message, sourceNodes } = await queryEngine.query({
+    query: "What this file is all about?",
+  });
+
+  // Output response with sources
+  console.log(message.content);
+
+  if (sourceNodes) {
+    sourceNodes.forEach((source: NodeWithScore, index: number) => {
+      console.log(
+        `\n${index}: Score: ${source.score} - ${source.node.getContent(MetadataMode.NONE).substring(0, 50)}...\n`,
+      );
+    });
+  }
 }
 
 main().catch(console.error);
